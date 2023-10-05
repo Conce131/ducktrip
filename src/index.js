f3cb2085b2d53a77318b383e229e98bc8e931f8b
Elena Expósito <elexla97@gmail.com>
2023-10-05 14:05:54 +0200

    functionality cards section 3

--- src/index.js
+++ src/index.js
@@ -23,21 +23,3 @@ document.querySelector('.search-button').addEventListener('click', searchInGoogl
 // luego si se hace click en esa clase se llamara la funcion searchInGoogle
 
 //
-
-// ? guardamos en una variable el div de las tarjetas de la seccion vuelos
-const section3Cards = document.querySelectorAll('div.section3-card');
-// como devuelve un array de div entonces hacemos un forEach para recorrerlo y asi hacer el
-// evento click en cada Card llamando a searchInGoogleImg con this refiriendose a esta card
-section3Cards.forEach((card) => {
-  card.addEventListener('click', (event) => {
-    // Aqui guardamos en infoTitle justo el texto que tiene .info-tittle al que clicas
-    const infoTitle = event.currentTarget.querySelector('.info-title').textContent;
-    searchInGoogleImg(infoTitle);
-  });
-});
-// se le pasa por parametro la informacion del contendor que clicamos guarda
-// en una variable el texto de .info-tittle y hace una busqueda con ese texto
-const searchInGoogleImg = (infoTitle) => {
-  console.log(infoTitle);
-  window.open(`https://www.google.com/search?q=${infoTitle}`);
-};
