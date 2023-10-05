ea3be77e083678aaa0b0837ab727d03c3fd6e491
Carlos Concepcion <concep131@gmail.com>
2023-10-05 13:42:00 +0100

    const isGitHubPages = false;
    Vite.config const isGitHubPages now is false for netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import path from "node:path";
+import { defineConfig } from 'vite';
+import path from 'node:path';
 
-const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+const isGitHubPages = false;
+const folderName = `${path.basename(process.cwd())}/`;
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const base = mode === 'production' && isGitHubPages ? `/${folderName}` : '/';
 
 export default defineConfig({
-  root: "src",
+  root: 'src',
   base,
   mode,
-  envDir: "../",
-  publicDir: "../public",
+  envDir: '../',
+  publicDir: '../public',
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname
-    }
+      '@': new URL('./src', import.meta.url).pathname,
+    },
   },
   build: {
-    outDir: "../dist",
-    assetsDir: "./"
-  }
+    outDir: '../dist',
+    assetsDir: './',
+  },
 });
